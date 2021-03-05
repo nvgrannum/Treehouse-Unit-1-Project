db139d68767cbd3c6ec165bd5f0589ae0d49a372
nvgrannum <79998374+nvgrannum@users.noreply.github.com>
2021-03-05 15:06:08 -0800

    Update script.js

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -11,19 +11,59 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
+let quotes = [
+  {
+    quote: 'What',
+    source: 'me',
+    citation: 'my head',
+    year: 1993
+  },
+  {
+    quote: 'who',
+    source: 'me'
+  },
+  {
+    quote: 'why',
+    source: 'me'
+  },
+  {
+    quote: 'Dont judge me',
+    source: 'You'
+  },
+  {
+    quote: 'But I am already judging',
+    source: 'me'
+  },
+  {
+    quote: 'Once upon a time',
+    source: 'every childrens book ever'
+  },
+  {
+    quote: 'I will get a good grade in therapy. This is something both reasonable to want and possible to achieve',
+    source: 'some random person'
+  }
+  
+];
 
 
 /***
  * `getRandomQuote` function
 ***/
 
-
+function getRandomQuote() {
+  
+}
 
 /***
  * `printQuote` function
 ***/
 
+function printQuote() { `
+  <p class="quote"></p>
+  <p class="source"></p>
 
+`
+}
 
 /***
  * click event listener for the print quote button
